111117ab4adc5424bc2f31b2bbe343820cb5543f
omkarp7338 <omkarp7338@gmail.com>
2026-08-15 10:53:55 +0530

    neon db check added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,24 +1,30 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-  console.error('DATABASE_URL is not defined in the environment variables.');
+  console.error("DATABASE_URL is not defined in the environment variables.");
   process.exit(1);
 }
 
-const isCloudDb = databaseUrl.includes('render.com') || databaseUrl.includes('supabase') || databaseUrl.includes('dpg-');
-
+const isCloudDb =
+  databaseUrl.includes("render.com") ||
+  databaseUrl.includes("supabase") ||
+  databaseUrl.includes("neon.tech") ||
+  databaseUrl.includes("dpg-");
 const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'postgres',
+  dialect: "postgres",
   logging: false, // Set to console.log for SQL query debugging
-  dialectOptions: (process.env.NODE_ENV === 'production' || isCloudDb) ? {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  } : {}
+  dialectOptions:
+    process.env.NODE_ENV === "production" || isCloudDb
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 });
 
 module.exports = sequelize;
